978cf1549acfe45c543585073bd117510e7f5a5e
Samuel Ekholm <samuelekholm3@gmail.com>
2021-09-08 12:15:54 +0200

    added cors

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { Socket } from 'socket.io';
+var cors = require('cors');
 
 const app = express();
+app.use(cors())
 const port = process.env.PORT || 3001;
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, { cors: { origin: '*' } });
